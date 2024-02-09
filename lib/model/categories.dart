c3d375a4ba335213824be92e7b78222bf4231a87
saravanan-sarav <saravanan.subramani@kumaran.com>
2024-02-09 17:31:17 +0530

    Home Page UI Completed.

--- lib/model/categories.dart
+++ lib/model/categories.dart
@@ -1,20 +1,19 @@
-import 'dart:ffi';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-class CategoryList{
+class CategoryList {
   final num categoryId;
   final String taskNumber;
   final String categoryName;
   final double percent;
   final Color color;
-  CategoryList(this.categoryId,this.categoryName, this.percent, this.color, this.taskNumber);
+
+  CategoryList(this.categoryId, this.categoryName, this.percent, this.color,
+      this.taskNumber);
 }
 
- final List<CategoryList> categories = [
-  CategoryList(1,"Business", 0.8, Colors.yellow,"30"),
-  CategoryList(2,"Productivity", 0.2,Colors.green,"50"),
-  CategoryList(3,"Entertainment", 0.3,Colors.blue,"20"),
-  CategoryList(4,"Food", 0.8,Colors.yellow,"10"),
+final List<CategoryList> categories = [
+  CategoryList(1, "Business", 0.8, Colors.yellow, "30"),
+  CategoryList(2, "Productivity", 0.2, Colors.green, "50"),
+  CategoryList(3, "Entertainment", 0.3, Colors.blue, "20"),
+  CategoryList(4, "Food", 0.8, Colors.yellow, "10"),
 ];
